6da8ea98c22d78cdb11382497787984826500b7d
Pavubrar <56096198+Pavubrar@users.noreply.github.com>
2023-09-10 13:27:42 -0600

    index.css imported in main.jsx

--- pluralistic/src/main.jsx
+++ pluralistic/src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-
+import App from './App.jsx';
+import './index.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 ReactDOM.createRoot(document.getElementById('root')).render(
